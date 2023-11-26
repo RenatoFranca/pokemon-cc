270f853ddde9bf74f506bdf4d77b21d62a07fe6a
Renato França <renatofrancarpmf@gmail.com>
2023-11-26 10:15:03 +0000

    Folder structure change

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,13 @@
+import PokemonList from "../_components/PokemonList";
+
+const App = () => {
+  return (
+    <>
+      <h1>Pokemon CC</h1>
+
+      <PokemonList />
+    </>
+  );
+};
+
+export default App;
--- app/components/home/home.types.ts
+++
@@ -1,3 +0,0 @@
-export type HomeLayoutProps = {
-  children: React.ReactNode;
-};
